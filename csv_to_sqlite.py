75a4009c49fdb8dc5e726f5184b0ae6af50a2de3
nluetzge <nluetzge@gmail.com>
2024-08-20 13:16:39 +0200

    - adding a new routine to convert the csv files to a database - added a new folder with that database

---
+++ csv_to_sqlite.py
@@ -0,0 +1,24 @@
+# IDENT			code.py
+# LANGUAGE		Python
+# AUTHOR		N. LUETZGENDORF
+# PURPOSE		
+#
+# VERSION
+# 1.0.0 DD.MM.YYYY NL Creation
+# ===============================================================
+# Imports
+# ===============================================================
+from acronym_gui import read_acronyms
+import pandas as pd
+import sqlite3
+
+filename = 'tables/LISA_acronyms.csv'
+tb_all = read_acronyms(filename)
+
+df = tb_all.to_pandas()
+
+conn = sqlite3.connect('acronyms.db')
+
+df.to_sql('table_name', conn, if_exists='replace', index=False)
+conn.commit()
+conn.close()
